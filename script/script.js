609dbf6d9d375b6d688c40e92300530daaf4c6f8
Folky63 <folky63@gmail.com>
2016-12-15 01:01:10 +0300

    add riddle

--- script/script.js
+++ script/script.js
@@ -1,16 +1,4 @@
-// var array = [10, 20, 30, 40];
-// document.write('line 17: array = ' + array + '<br>');
-// var summ = 0;
-// document.write('line 19: summ = ' + summ + '<br>');
-// document.write('<b>actual voodoo starts here:</b><br>')
-// for (var counter = 0; counter < array.length; counter++) {
-//     document.write('line 23: counter = ' + counter + '<br>');
-//     document.write('line 25: summ = ' + array[counter] + '+' + summ + '<br>');
-//     summ += array[counter]; //b = 0+40 ?
-//     document.write('line 25: summ = ' + summ + '<br>');
-// }
-// document.write('<b>the result of your this voodoo is ' + summ + '</b>');
-//------------------------------
+/* currency */
 var usdbyn = 1.9526;
 var eurbyn = 2.0765;
 var rubbyn = 0.031966;
@@ -25,6 +13,38 @@ var outputbox_usd = document.querySelector('#outputusd');
 var outputbox_eur = document.querySelector('#outputeur');
 var outputbox_rub = document.querySelector('#outputrub');
 var outputboxes = [outputbox_usd, outputbox_eur, outputbox_rub];
+/* */
+/* riddle */
+var riddle1 = 'Где люди платят за то, что у них отнимают?';
+var correct1 = 'Парикмахерская';
+var riddle2 = 'У отца Мэри есть 5 дочерей: Чача, Чичи, Чече, Чочо. Как зовут 5 дочь?';
+var correct2 = 'Мэри';
+var riddle3 = 'На дне моря лежит сундук. В нем все есть, кроме одного. Чего в нем нет?';
+var correct3 = 'Пустоты';
+var riddle4 = 'Завязать можно, а развязать нельзя.';
+var correct4 = 'Разговор';
+var count = 0;
+var button = document.querySelector('.riddle');
+
+function insert_fancy_name_here(riddle, correct) {
+    var answer = prompt(riddle);
+    console.log(answer.toLowerCase());
+    console.log(correct.toLowerCase());
+    if (answer.toLowerCase() === correct.toLowerCase()) {
+        alert('Верно. Счет: ' + (++count));
+    }
+    else {
+        alert('Неа. Счет: ' + count);
+    };
+    button.innerHTML = ('Счет: ' + count);
+};
+button.addEventListener('click', function (ev) {
+    ev.preventDefault();
+    insert_fancy_name_here(riddle1, correct1);
+    insert_fancy_name_here(riddle2, correct2);
+    insert_fancy_name_here(riddle3, correct3);
+    insert_fancy_name_here(riddle4, correct4);
+});
 
 function calccurrency(currency, output) {
     input = input_byn.value;
@@ -50,3 +70,16 @@ submit_byn.addEventListener('click', function (ev) {
 //    output_byn_usd.classList.add('show');
 //    output_byn_usd.innerHTML = result;
 //});
+// var array = [10, 20, 30, 40];
+// document.write('line 17: array = ' + array + '<br>');
+// var summ = 0;
+// document.write('line 19: summ = ' + summ + '<br>');
+// document.write('<b>actual voodoo starts here:</b><br>')
+// for (var counter = 0; counter < array.length; counter++) {
+//     document.write('line 23: counter = ' + counter + '<br>');
+//     document.write('line 25: summ = ' + array[counter] + '+' + summ + '<br>');
+//     summ += array[counter]; //b = 0+40 ?
+//     document.write('line 25: summ = ' + summ + '<br>');
+// }
+// document.write('<b>the result of your this voodoo is ' + summ + '</b>');
+//------------------------------
